f04433890431ce7cee1c8ab193acfd5814a85958
Sithila2010 <chutimalli182@gmail.com>
2023-08-28 17:41:46 +0200

    Changes must be commited!

--- 02-Fundamentals Part 2/script.js
+++ 02-Fundamentals Part 2/script.js
@@ -24,8 +24,19 @@ const fullName = function(fName, sName){
     console.log(`${fName} ${sName}`);
 }
 fullName("Sithila Sihan", "Somaratne")
+/*Arrow Functions*/
 const theName = (fName, sName) => {
     return `${fName} ${sName}`;
 }
 let Name = theName("Sithila Sihan", "Somaratne");
 console.log("My full name is "+Name);
+const isPrime = (number) =>{
+    if(number > 0 && number > 1){
+        if(number % 2 == 0 && number != 2){
+            console.log("Number is not prime");
+        }else{
+            console.log("Number is  prime")
+        }
+    }
+}
+isPrime(2);
