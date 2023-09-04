a9edc8f7f8a76be7062724016b1f2215a88625dc
Sithila2010 <chutimalli182@gmail.com>
2023-09-04 18:11:31 +0200

    Changes must be committed!

--- 03-DOM/script.js
+++ 03-DOM/script.js
@@ -1,10 +1,13 @@
 let checkIsAEmail = () =>{
+    let btn = document.querySelector('#btn');
     let email = document.getElementById('email').value;
     if (String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
         alert("You've entered a valid email.");
+        btn.setAttribute("class", "btn btn-success");
         document.getElementById('result').innerHTML = email;
     } else {
         alert("Sorry! You've entered an invalid email");
+        btn.setAttribute("class", "btn btn-danger");
         document.getElementById('result').innerHTML = "Wrong Email!";
     }
 }
@@ -19,3 +22,22 @@ const body = document.getElementsByClassName('theBody');
 console.log(body);
 const label = document.getElementsByTagName('label');
 console.log(label);
+console.log(document.querySelector('#title'));
+const listOfSpan = document.querySelectorAll('#span');
+console.log(listOfSpan);
+listOfSpan.forEach(element =>{
+    element.innerHTML = "This is a span, not a spam!";
+});
+document.querySelector('#title').innerHTML = "Welcome to Email Validator!";
+let strong = document.querySelector('strong');
+strong.innerHTML = "I am bold text, not strong!";
+strong.setAttribute("class", "btn btn-primary");
+strong.innerHTML = "I am button!";
+strong.classList.remove("btn", "btn-primary");
+strong.innerHTML = "I am bold text!";
+let title = document.querySelector('#title');
+title.style.fontSize = "40px";
+title.style.fontWeight = "600";
+title.style.backgroundColor = "black";
+title.style.color = "blue";
+title.style["color"] = "red";
