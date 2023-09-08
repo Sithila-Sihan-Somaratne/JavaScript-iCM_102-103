a7ea5aa0eed2a3832abfeee777c2e3223b572bca
Sithila2010 <chutimalli182@gmail.com>
2023-09-08 18:05:07 +0200

    Changes must be commited!

--- 04-jQuery/app.js
+++ 04-jQuery/app.js
@@ -1,1 +1,24 @@
+//Select by tag name
+console.log(jQuery('h1'));
+console.log($('button'));
+let titleTag = jQuery('title');
+console.log(titleTag);
+//Select by class
+let sbc = $('.btn-danger');
+console.log(sbc);
+//Select by id
+let sbi = $('#sv-btn');
+console.log(sbi);
+///////////////////////
+let sp = $('button.btn#sv-btn');
+console.log(sp);
+console.log($('button').not('.btn-danger'));
+console.log($('button').filter(':odd'));
+////////////////////////////////////////////////////////
+// perform actions
+///////////////////////////////////////////////////////
+//Set Text Content
+$('h1').text('Sri Lanka');
+
+//Get Text Content
 console.log($('h1').text());
